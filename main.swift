93530ff00fc4b9e45ce06ba54f762136ed7d14ba
Matvey Emelyanov <kassius2008@gmail.com>
2024-02-28 00:03:53 +0400

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+let name="Matvey"
+let surName="Emelyanov"
+let age = 21
+let sex="Male"
+let city="Saint-Petersburg"
+
+print("first name: \(name) \nsurname: \(surName) \nage: \(age) \nsex: \(sex) \ncity - \(city)")
